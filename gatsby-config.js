f34689029a6f7ac3ad927d9209d3468027c18eb6
Li Lin <Li.Lin@openwindows.com.au>
2020-02-24 17:18:07 +1100

    Replace manifest ico with jpg

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/favicon.ico`,
+        icon: `content/assets/manifest-pic.jpg`,
       },
     },
     `gatsby-plugin-react-helmet`,
